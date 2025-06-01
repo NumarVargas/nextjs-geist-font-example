a24ed55122c74634f7107f7194dd862ce3451b63
NumarVargas <141415544+NumarVargas@users.noreply.github.com>
2025-05-31 23:06:14 -0500

    Add .next/static/chunks/src_app_page_tsx_cabe5e9a._.js 1748750817768

---
+++ .next/static/chunks/src_app_page_tsx_cabe5e9a._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_e9afc70f._.js"
+],
+    source: "dynamic"
+});
